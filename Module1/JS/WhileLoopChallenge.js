67a88c3b947f38cf9092eebd645f768f74771e56
Kobe <kobe.wright2017@gmail.com>
2022-08-25 13:28:11 -0400

    While loop challenge part 1 and 2

---
+++ Module1/JS/WhileLoopChallenge.js
@@ -0,0 +1,21 @@
+let count = 2;
+// ADD CODE HERE
+while(count < 8){
+    count += 2
+}
+
+//Uncomment these to check your work!
+console.log(count);
+
+// ADD CODE HERE
+let addThis = 0
+let sum = 0
+
+while(addThis < 10){
+    sum += addThis
+    addThis++
+}
+
+
+// Uncomment the line below to check your work!
+console.log(sum);
