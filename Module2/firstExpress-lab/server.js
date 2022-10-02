9f4118fe9a7af4bf5c476804282818b12875aeb2
Kobe <kobe.wright2017@gmail.com>
2022-10-02 17:55:56 -0400

    Magic 8 Ball express application created.

--- Module2/firstExpress-lab/server.js
+++ Module2/firstExpress-lab/server.js
@@ -19,6 +19,13 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`Tip: ${tip}`)
 })
 
+const phrasesArr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+app.get('/magic/:phrase', (req, res) => {
+    const {phrase} = req.params;
+    let magResponse = phrasesArr[Math.floor(Math.random() * 20)];
+    res.send(`Question: ${phrase} <h1>Magic 8 Ball Response: ${magResponse}</h1>`)
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 })
