8ef548f9a990f853a23ce959076ab6406d084b80
Kobe <kobe.wright2017@gmail.com>
2022-10-18 14:00:57 -0400

    New changes

--- Module2/blogApp/app.js
+++ Module2/blogApp/app.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(morgan('dev'))
